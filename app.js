a5609ab2a9078d94685e319e22257b88deff361e
Tony Baldessari <tonybalde@hotmail.com>
2023-11-28 20:00:04 -0300

    project finished locally

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var bodyParser = require('body-parser');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -26,6 +28,8 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // error handler
 app.use(function(err, req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,83 @@
+// // // const messages = [
+// // //   {
+// // //     text: "Hi there!",
+// // //     user: "Amando",
+// // //     added: new Date()
+// // //   },
+// // //   {
+// // //     text: "Hello World!",
+// // //     user: "Charles",
+// // //     added: new Date()
+// // //   }
+// // // ];
+
+// // // var express = require('express');
+// // // var router = express.Router();
+
+// // // /* GET home page. */
+// // // router.get('/', function(req, res, next) {
+// // //   res.render('index', { title: 'Mini Message Board' });
+// // // });
+
+// // // module.exports = router;
+
+// // var express = require('express');
+// // var router = express.Router();
+
+// // const messages = [
+// //   {
+// //     text: "Hi there!",
+// //     user: "Amando",
+// //     added: new Date()
+// //   },
+// //   {
+// //     text: "Hello World!",
+// //     user: "Charles",
+// //     added: new Date()
+// //   }
+// // ];
+
+// // /* GET home page. */
+// // router.get('/', function(req, res, next) {
+// //   res.render('index', { title: 'Mini Message Board', messages: messages });
+// // });
+
+// // module.exports = router;
+
+// var express = require('express');
+// var router = express.Router();
+
+// /* Existing messages array */
+// const messages = [
+//   {
+//     text: "Hi there!",
+//     user: "Amando",
+//     added: new Date()
+//   },
+//   {
+//     text: "Hello World!",
+//     user: "Charles",
+//     added: new Date()
+//   }
+// ];
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Mini Message Board', messages: messages });
+// });
+
+// /* GET new message form. */
+// router.get('/new', function(req, res, next) {
+//   res.render('form', { title: 'New Message Form' });
+// });
+
+// module.exports = router;
+
+
+var express = require('express');
+var router = express.Router();
+
+/* Existing messages array */
 const messages = [
   {
     text: "Hi there!",
@@ -11,12 +91,33 @@ const messages = [
   }
 ];
 
-var express = require('express');
-var router = express.Router();
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mini Message Board' });
+  res.render('index', { title: 'Mini Message Board', messages: messages });
+});
+
+/* GET new message form. */
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'New Message Form' });
+});
+
+/* POST new message. */
+router.post('/new', function(req, res, next) {
+  // Assuming you have a form with input fields named 'author' and 'message'
+  const author = req.body.author;
+  const messageText = req.body.message;
+
+  // Add a new message to the messages array
+  const newMessage = {
+    text: messageText,
+    user: author,
+    added: new Date()
+  };
+
+  messages.push(newMessage);
+
+  // Redirect to the home page after adding the new message
+  res.redirect('/');
 });
 
 module.exports = router;
