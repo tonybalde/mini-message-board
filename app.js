8a8352bebc1d29de86ec125e8bdd395dcbf57ce3
Tony Baldessari <tonybalde@hotmail.com>
2023-11-29 20:06:06 -0300

    Check app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 require('dotenv').config(); // Load environment variables from .env file
 
+var mongoose = require('mongoose');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
